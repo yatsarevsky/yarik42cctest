cbbd07b5db731fdb2a6f69e31884e08551cc263d
Yarik85 <yatsarevsky@gmail.com>
2013-02-27 15:21:33 +0200

    ticket:9 add file out

--- yaproject/vcard/management/commands/show_models.py
+++ yaproject/vcard/management/commands/show_models.py
@@ -2,28 +2,28 @@ from django.core.management.base import BaseCommand
 from django.db.models import get_app, get_models
 from django.conf import settings
 
-from StringIO import StringIO
-
 import sys
+import datetime
 
 
 class Command(BaseCommand):
     exclude_apps = ('django_coverage', 'staticfiles', 'messages')
 
     def handle(self, *args, **kwargs):
-        sys.stderr = StringIO()
+        file_name = '%s.dat' % str(datetime.date.today())
+        l = []
 
         for app_path in settings.INSTALLED_APPS:
             app_name = app_path.split('.')[-1]
             if app_name not in self.exclude_apps:
-                out = '[%s]\n' % app_name
-                print out
-                sys.stderr.write('error:\n')
-                sys.stderr.write(out)
                 app = get_app(app_name)
 
                 for model in get_models(app):
                     cnt = model.objects.count()
-                    out = '\t%s (%d)' % (model.__name__, cnt)
-                    print out
-                    sys.stderr.write(out)
+                    out = '%s(%d) ' % (model.__name__, cnt)
+                    sys.stderr.write('[error]' + out + '\n')
+                    l.append(out)
+
+        handle = open(file_name, 'w+')
+        handle.write(str(l))
+        handle.close()
