7614d64d5abf8ce0a949d3ee1fe6aed04a77dfb5
Yarik85 <yatsarevsky@gmail.com>
2013-03-07 12:30:09 +0200

    ticket:9 add bash script

--- yaproject/show_models.sh
+++ yaproject/show_models.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python manage.py show_models --tee-stderr 2> $(date '+%Y-%m-%d').dat
+python manage.py show_models 2> $(date '+%Y-%m-%d').dat
--- yaproject/vcard/management/commands/show_models.py
+++ yaproject/vcard/management/commands/show_models.py
@@ -3,16 +3,12 @@ from django.db.models import get_app, get_models
 from django.conf import settings
 
 import sys
-import datetime
 
 
 class Command(BaseCommand):
     exclude_apps = ('django_coverage', 'staticfiles', 'messages')
 
     def handle(self, *args, **kwargs):
-        file_name = '%s.dat' % str(datetime.date.today())
-        l = []
-
         for app_path in settings.INSTALLED_APPS:
             app_name = app_path.split('.')[-1]
             if app_name not in self.exclude_apps:
@@ -22,8 +18,3 @@ class Command(BaseCommand):
                     cnt = model.objects.count()
                     out = '%s(%d) ' % (model.__name__, cnt)
                     sys.stderr.write('[error]' + out + '\n')
-                    l.append(out)
-
-        handle = open(file_name, 'w+')
-        handle.write(str(l))
-        handle.close()
